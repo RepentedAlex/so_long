b5a25a7bc39f90b966e5d3e3036eb7ee2410ba10
Alessandro Petitcollin <petitcollin.alessandro@gmail.com>
2024-06-12 16:52:10 +0200

    Added line to enable test for all map-related things (need to write the examples)

--- examples/src/examples.c
+++ examples/src/examples.c
@@ -18,6 +18,8 @@ int	main(int argc, char *argv[])
 	(void)argc;
 	if (!strcmp(argv[1], "display"))
 		return (ft_test_displaying(), 0);
+	if (!strcmp(argv[1], "map"))
+		return (0);
 	if (!strcmp(argv[1], "loop"))
 		return (0);
 	if (!strcmp(argv[1], "data"))
